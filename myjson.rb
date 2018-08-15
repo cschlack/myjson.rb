c2e2e91ace72d05da0ea5d71c306feb679235b04
Christian Schlack <christian@backhub.co>
2018-08-15 16:22:23 +0200

    Refactoring

--- myjson.rb
+++ myjson.rb
@@ -1,65 +1,93 @@
 require 'oj'
 
-class MyJSON
-  include Enumerable
-
-  # json: JSON String
-  # position: starting element in the json string (eg '/3/user/login')
-  def initialize(json, position=nil)
-    @json = json
-    @position = position
-  end
-
-  # dumps the original json string
-  def dump
-    with_json do |node|
-      @position ? node.dump(@position) : @json
+# This module implements a lazy JSON reader, which parses the JSON-String
+# when a property is being accessed
+module MyJSON
+  def self.parse(json, position = nil)
+    instance = MyJSON::Any.new(json, position)
+    case instance.type
+    when 'Array'
+      MyJSON::Array.new(json, position)
+    when 'Hash'
+      MyJSON::Hash.new(json, position)
+    else
+      instance
     end
   end
 
-  def each
-    with_json do |node|
-      if node.type == Array
+  class Any
+    # json: JSON String
+    # position: starting element in the json string (eg '/3/user/login')
+    def initialize(json, position = nil)
+      @json = json
+      @position = position
+    end
+
+    # dumps the original json string
+    def dump
+      execute_within_oj(&:dump)
+    end
+
+    def fetch
+      execute_within_oj(&:fetch)
+    end
+
+    def [](name = nil)
+      execute_within_oj do |node|
+        node.move(name)
+        MyJSON.parse(@json, node.where?).fetch
+      end
+    end
+
+    def key?(key)
+      respond_to_missing?(key)
+    end
+
+    def type
+      execute_within_oj(&:type).to_s
+    end
+
+    private
+
+    def execute_within_oj
+      Oj::Doc.open(@json) do |doc|
+        doc.move(@position) if @position
+        yield(doc)
+      end
+    end
+
+    def method_missing(method_name, *arguments, &block)
+      self[method_name.to_s]
+    rescue ArgumentError
+      super
+    end
+
+    def respond_to_missing?(method_name, _include_private = false)
+      execute_within_oj do |node|
+        method_name ? !node.type(method_name.to_s).nil? : super
+      end
+    end
+  end
+
+  class Array < Any
+    include Enumerable
+
+    def each
+      execute_within_oj do |node|
         node.each_child { |child| yield(self[child.where?]) }
-      elsif node.type == Hash
+      end
+    end
+
+    def fetch
+      self
+    end
+  end
+
+  class Hash < Array
+    def each
+      execute_within_oj do |node|
         node.each_child { |child| yield(child.local_key, self[child.where?]) }
       end
-    end
-  end
-
-  def fetch
-    with_json { |node| node.fetch }
-  end
-
-  def method_missing(name = nil)
-    with_json do |node|
-      begin
-        # move to the starting JSON element
-        node.move(name.to_s) if name
-
-        if [Array, Hash].include? node.type
-          self.class.new(@json, node.where?)
-        else
-          node.fetch
-        end
-      rescue ArgumentError
-        nil
-      end
-    end
-  end
-
-  def key?(key)
-    ! self[key].nil?
-  end
-
-  alias_method :[], :method_missing
-
-  private
-
-  def with_json
-    Oj::Doc.open(@json) do |doc|
-      doc.move(@position) if @position
-      yield(doc)
     end
   end
 end
--- myjson_spec.rb
+++ myjson_spec.rb
@@ -2,16 +2,16 @@ require './myjson'
 
 describe MyJSON do
   it 'creates an instance' do
-    expect(MyJSON.new('')).to be_an_instance_of MyJSON
+    expect(MyJSON.parse('')).to be_a_kind_of MyJSON::Any
   end
 
   it 'parses a JSON string' do
-    json = MyJSON.new('[{"a":1, "b":2}]')
-    expect(json).to be_an_instance_of MyJSON
+    json = MyJSON.parse('[{"a":1, "b":2}]')
+    expect(json).to be_a_kind_of MyJSON::Any
   end
 
   it 'accesses properties by name' do
-    json = MyJSON.new('{ "id":1000, "owner":{ "id":9999, "login":"superman" }}')
+    json = MyJSON.parse('{ "id":1000, "owner":{ "id":9999, "login":"superman" }}')
 
     expect(json.id).to eq 1000
     expect(json.owner.id).to eq 9999
@@ -19,7 +19,7 @@ describe MyJSON do
   end
 
   it 'accesses properties by name inside an array' do
-    array = MyJSON.new('[{ "owner":{ "login":"jekyll" }}, { "owner":{ "login":"hyde" }}]').to_a
+    array = MyJSON.parse('[{ "owner":{ "login":"jekyll" }}, { "owner":{ "login":"hyde" }}]').to_a
 
     expect(array.first.owner.login).to eq 'jekyll'
     expect(array.last.owner.login).to eq 'hyde'
@@ -27,78 +27,73 @@ describe MyJSON do
 
   describe '.dump' do
     it 'returns the original string' do
-      json_string = '[{"a":1, "b":2}]'
-      expect(MyJSON.new(json_string).dump).to be json_string
+      json_string = '[{"a":1,"b":2}]'
+      expect(MyJSON.parse(json_string).dump).to eq json_string
     end
 
     it 'returns a substring' do
-      json = MyJSON.new('{"a":{"b":2}}')
+      json = MyJSON.parse('{"a":{"b":2}}')
       expect(json.a.dump).to eq '{"b":2}'
     end
   end
 
   describe '.key?' do
     it 'finds an existing key' do
-      json = MyJSON.new('{ "id":1000, "owner":{ "id":9999, "login":"superman" }}')
+      json = MyJSON.parse('{ "id":1000, "owner":{ "id":9999, "login":"superman" }}')
       expect(json.key?('owner')).to be true
     end
 
     it "doesn't find a non-existing key" do
-      json = MyJSON.new('{ "id":1000, "owner":{ "id":9999, "login":"superman" }}')
+      json = MyJSON.parse('{ "id":1000, "owner":{ "id":9999, "login":"superman" }}')
       expect(json.key?('organizations')).to be false
     end
   end
 
   describe '.to_a' do
     it 'returns an array' do
-      array = MyJSON.new('[1, 2, 3, 4]').to_a
+      array = MyJSON.parse('[1, 2, 3, 4]').to_a
       expect(array).to be_an_instance_of Array
       expect(array.size).to eq 4
-    end
-
-    it 'returns [] if it is not an array' do
-      not_an_array = MyJSON.new('true').to_a
-      expect(not_an_array).to eq []
     end
   end
 
   describe '.fetch' do
     it 'returns an array' do
-      expect(MyJSON.new('[]').fetch).to be_an_instance_of Array
+      expect(MyJSON.parse('[]').fetch).to be_an_instance_of MyJSON::Array
     end
 
     it 'returns a hash' do
-      expect(MyJSON.new('{}').fetch).to be_an_instance_of Hash
+      expect(MyJSON.parse('{}').fetch).to be_an_instance_of MyJSON::Hash
     end
   end
 
   describe '.get_value' do
     it 'returns a number' do
-      expect(MyJSON.new('[1]').first).to be_an_instance_of Fixnum
+      expect(MyJSON.parse('[1]').first).to be_an_instance_of Integer
     end
 
     it 'returns a string' do
-      expect(MyJSON.new('["string"]').first).to be_an_instance_of String
+      expect(MyJSON.parse('["string"]').first).to be_an_instance_of String
     end
 
     it 'returns true' do
-      expect(MyJSON.new('[true]').first).to be true
+      expect(MyJSON.parse('[true]').first).to be true
     end
 
     it 'returns false' do
-      expect(MyJSON.new('[false]').first).to be false
+      expect(MyJSON.parse('[false]').first).to be false
     end
 
     it 'returns nil' do
-      expect(MyJSON.new('[null]').first).to be nil
+      expect(MyJSON.parse('[null]').first).to be nil
     end
 
     it 'returns a MyJSON instance for an array' do
-      expect(MyJSON.new('[[]]').first).to be_an_instance_of MyJSON
+      expect(MyJSON.parse('[[]]').first).to be_a_kind_of MyJSON::Any
     end
 
     it 'returns a MyJSON instance for a hash' do
-      expect(MyJSON.new('[{}]').first).to be_an_instance_of MyJSON
+      expect(MyJSON.parse('[{}]').first).to be_a_kind_of MyJSON::Any
     end
   end
 end
